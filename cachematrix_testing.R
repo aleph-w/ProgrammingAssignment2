71d7fb1c071e0e479d232dab002fb595a5fc40a2
Matt Warpinski <aleph-w@users.noreply.github.com>
2015-02-13 11:36:55 -0700

    Create cachematrix_testing.R

---
+++ cachematrix_testing.R
@@ -0,0 +1,21 @@
+## This R script contains two functions: makeCacheMatrix and cacheSolve
+## makeCacheMatrix creates a special metrix object that can cache its own inverse
+## cacheSolve computes the inverse of the special matrix returned by the 
+## makeCacheMatrix function. If the inverse has already been calculated and the 
+## matrix has not changed, then it will retrieve the inverse from the cache
+
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  
+  
+  
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+}
