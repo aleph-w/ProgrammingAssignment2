cd8353dee789ae361f6e8d0f2c60d992832bd0c7
Matt Warpinski <aleph-w@users.noreply.github.com>
2015-02-18 13:21:33 -0700

    Update cachematrix.R
    Added in code and error testing

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R script contains one function: cacheSolve
+## The prupose of this is to cache computationally intensive inverses for
+## any possible future use.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+#### Note that the first function is not used. ####
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the special matrix returned by the 
+## makeCacheMatrix function. If the inverse has already been calculated and the 
+## matrix has not changed, then it will retrieve the inverse from the cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) 
+{
+  # Create matrix_cache if one does not exits
+  if (!exists("matrix_cache"))
+  {
+    matrix_cache <<- list()
+  }
+  
+  # This part looks into the matrix_cache list to find the matrix in
+  # question, and return the ith + 1 value which corresponds to the
+  # inverse
+  if (list(x) %in% matrix_cache)
+  {
+    for (i in 1:length(matrix_cache))
+    {
+      if (matrix_cache[i] %in% list(x))
+      {
+        message("Found it!!")
+        return(matrix_cache[i+1])
+        break
+      }
+      
+    }
+  }
+  # Need to make sure the matirx is invertible
+  if (det(x) != 0)
+  {
+    matrix_inverse <- solve(x)
+    temp_cache <- c(list(x), list(matrix_inverse))
+    matrix_cache <<- c(matrix_cache, temp_cache)
+    return(matrix_inverse)
+  }
+  
+  # Let the user know if the matrix is not invertible
+  if (det(x) == 0)
+  {
+    message("That matrix is not invertible")
+  }
 }
