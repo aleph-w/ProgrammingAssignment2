950894023737cb1c85a78806e36719a6e48df6a8
aleph-w <aleph-w@users.noreply.github.com>
2015-02-18 19:04:45 -0700

    Update cachematrix.R
    Added some minor functionality. Edited comments to make the code more clear

--- cachematrix.R
+++ cachematrix.R
@@ -2,18 +2,17 @@
 ## The prupose of this is to cache computationally intensive inverses for
 ## any possible future use.
 
-#### Note that the first function is not used. ####
-
-
-## cacheSolve computes the inverse of the special matrix returned by the 
-## makeCacheMatrix function. If the inverse has already been calculated and the 
+## cacheSolve computes the inverse ofa matrix. If the inverse has already been calculated and the 
 ## matrix has not changed, then it will retrieve the inverse from the cache
+
+## There is no makeCacheMatrix function present as I found it to be unnecessary
 
 cacheSolve <- function(x) 
 {
   # Create matrix_cache if one does not exits
   if (!exists("matrix_cache"))
   {
+    # Needs to be created in the global environment, hence the '<<-'
     matrix_cache <<- list()
   }
   
@@ -33,9 +32,10 @@ cacheSolve <- function(x)
       
     }
   }
-  # Need to make sure the matirx is invertible
+  # Need to make sure the matrix is invertible
   if (det(x) != 0)
   {
+    message("Computing inverse..")
     matrix_inverse <- solve(x)
     temp_cache <- c(list(x), list(matrix_inverse))
     matrix_cache <<- c(matrix_cache, temp_cache)
